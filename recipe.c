6f5235567ffae2e919141ec91056f5b0eb7369fc
Makoto Shimazu <makoto.shimaz@gmail.com>
2014-07-06 14:03:32 +0900

    コードを読んだ感想と、スペースの統一

--- recipe.c
+++ recipe.c
@@ -1,30 +1,28 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
-int main (int argc, char* argv[]){
+int main ( int argc, char* argv[] ) {
   FILE *recipe_fp;
 
   /* Usageの表示 */
-  if (argc < 2){
-    fprintf ( stderr, "Usage:recipe [file_name]\n");
+  if ( argc < 2 ) {
+    fprintf( stderr, "Usage:recipe [file_name]\n" );
     return -1;
   }
 
   /* ファイルを開く */
   char *recipe_file_path = argv[1];
-  if ( ( recipe_fp = fopen( recipe_file_path , "r")) == NULL){
-    fprintf ( stderr, "ファイルが開けません\n");
+  if ( ( recipe_fp = fopen( recipe_file_path , "r" ) ) == NULL ) {
+    fprintf( stderr, "ファイルが開けません\n" );
     return -1;
   }
 
   /* 全てのレシピを出力 */
   char recipe_line[256];
   int recipe_id = 1;
-  while ( fscanf (recipe_fp, "%s", recipe_line) != EOF){
-
-    if ( strlen(recipe_line) == 0) continue;
-
-    printf ( "%d: %s\n", recipe_id, recipe_line);
+  while ( fscanf( recipe_fp, "%s", recipe_line ) != EOF ) {
+    if ( strlen( recipe_line ) == 0 ) continue;
+    printf ( "%d: %s\n", recipe_id, recipe_line );
     recipe_id++;
   }
 
